d6d291e237da3ca96a5662f10be479142002e251
Leonardo <leodemarchy@gmail.com>
2023-06-07 12:49:13 +0200

    Updated

--- src/test/java/it/unipd/dei/eis/domain/repositories/TheGuardianRepositoryTest.java
+++ src/test/java/it/unipd/dei/eis/domain/repositories/TheGuardianRepositoryTest.java
@@ -40,6 +40,6 @@ public class TheGuardianRepositoryTest {
     void pullData() throws Exception {
         List<ArticleModel> result = repository.pullData(context);
         assertEquals(result.size(), context.countArticles);
-       // assertEquals(result.get(0).id, 1121452019); da controllare perchè non ho la key
+        assertEquals(1121452019, result.get(0).id );
     }
 }
